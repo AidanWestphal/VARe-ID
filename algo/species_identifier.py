68296ecadb9d331ddf52dd974539d58e20db6fc8
Lawrence M <lawrencejunzh@gmail.com>
2025-02-19 16:25:03 -0500

    fix attribute in species identifier

--- algo/species_identifier.py
+++ algo/species_identifier.py
@@ -49,7 +49,7 @@ def clone_pyBioCLIP_from_github(directory, repo_url):
 def install_pyBioCLIP_from_directory(directory):
     try:
         print(f"Installing package from {directory}...")
-        subprocess.run([sys.executable, '-m', 'pip', 'install', directory], check=True)
+        subprocess.run([sys.executable, "-m", "pip", "install", directory], check=True)
         print("Package installed successfully...")
     except subprocess.CalledProcessError as e:
         print(f"Error installing package from {directory}: {e}")
@@ -77,7 +77,7 @@ def run_pyBioclip(bioclip_classifier, image_dir, df):
 
     for index, row in tqdm(df.iterrows()):
 
-        x0, y0, x1, y1 = ast.literal_eval(row["bbox"])
+        x0, y0, x1, y1 = ast.literal_eval(row["bbox pred"])
         image_filename = row["image uuid"]
 
         image_filepath = os.path.join(image_dir, f"{image_filename}.jpg")
@@ -121,7 +121,7 @@ def simplify_species(species_name, category_map):
 
 def postprerocess_dataframe(df):
 
-    # this is only when dectection has filter (ground truth) 
+    # this is only when dectection has filter (ground truth)
     # category_map_true = {"zebra_grevys": 0, "zebra_plains": 1, "neither": 2}
     # df["species_true_simple"] = df["annot species"].apply(
     #     lambda x: simplify_species(x, category_map_true)
