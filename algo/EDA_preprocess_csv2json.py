b633ad20371a6de1d2b0f01b9cd2703d3b5f8fe5
Lawrence M <lawrencejunzh@gmail.com>
2025-05-24 02:31:23 -0400

    edit image_detector

--- algo/EDA_preprocess_csv2json.py
+++ algo/EDA_preprocess_csv2json.py
@@ -1,6 +1,7 @@
 import argparse
 import json
-import uuid
+
+# import uuid
 import warnings
 
 import pandas as pd
@@ -53,6 +54,13 @@ def assign_viewpoints(df, excluded_viewpoints):
     return df
 
 
+def load_annotations_from_json(json_file_path):
+    with open(json_file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    return pd.DataFrame(data["annotations"])
+
+
 def convert_bbox(bbox_str):
     bbox_values = bbox_str.strip("[]").split(", ")
     return [float(value) for value in bbox_values]
@@ -78,9 +86,11 @@ if __name__ == "__main__":
     anno_path_csv = args.csv_file[sep:].replace("/", "")
     video_mode = args.video
 
-    df = pd.read_csv(
-        annot_dir + "/" + anno_path_csv, delimiter=",", quotechar='"', engine="python"
-    )
+    df = load_annotations_from_json(args.csv_file)
+
+    # df = pd.read_csv(
+    #     annot_dir + "/" + anno_path_csv, delimiter=",", quotechar='"', engine="python"
+    # )
 
     # annot uuid,image uuid,image fname,video path,frame number,bbox x,bbox y,bbox w,bbox h,
     # bbox pred score,category id,tracking id,timestamp,species_prediction,species_pred_score,species_pred_simple,
@@ -102,18 +112,16 @@ if __name__ == "__main__":
     # print(f"Number of annotations: {num_annotations}")
     # print(f"Number of images: {num_images}")
 
-    df["bbox"] = df["bbox"].apply(convert_bbox)
+    # # IF UUIDS AREN'T ALREADY PROVIDED (GT FILES), ADD THEM
+    # if "image uuid" not in df.columns or "uuid" not in df.columns:
+    #     image_uuid_map = {
+    #         image_path: str(uuid.uuid4()) for image_path in df["image_path"].unique()
+    #     }
+    #     # Add the UUIDs to the DataFrame
+    #     df["image_uuid"] = df["image_path"].map(image_uuid_map)
+    #     df["uuid"] = [str(uuid.uuid4()) for _ in range(len(df))]
 
-    # IF UUIDS AREN'T ALREADY PROVIDED (GT FILES), ADD THEM
-    if "image uuid" not in df.columns or "uuid" not in df.columns:
-        image_uuid_map = {
-            image_path: str(uuid.uuid4()) for image_path in df["image_path"].unique()
-        }
-        # Add the UUIDs to the DataFrame
-        df["image_uuid"] = df["image_path"].map(image_uuid_map)
-        df["uuid"] = [str(uuid.uuid4()) for _ in range(len(df))]
-
-    df["individual_id"] = df["individual_id_x"]
+    # df["individual_id"] = df["individual_id_x"]
 
     df = assign_viewpoints(df, excluded_viewpoints=["upback", "upfront"])
 
@@ -121,14 +129,14 @@ if __name__ == "__main__":
     if "category_id" not in df.columns:
         df["category_id"] = 0
 
-    # IF FRAME NUMBER NOT PROVIDED (GT)
-    if video_mode:
-        if "frame_number" not in df.columns:
-            df["frame_number"] = -1
-            for index, row in df.iterrows():
-                df.at[index, "frame_number"] = int(
-                    row["file_name"].split("_")[-1].split(".")[0]
-                )
+    # # IF FRAME NUMBER NOT PROVIDED (GT)
+    # if video_mode:
+    #     if "frame_number" not in df.columns:
+    #         df["frame_number"] = -1
+    #         for index, row in df.iterrows():
+    #             df.at[index, "frame_number"] = int(
+    #                 row["file_name"].split("_")[-1].split(".")[0]
+    #             )
 
     if video_mode:
         df_annotations_fields = [
@@ -186,6 +194,6 @@ if __name__ == "__main__":
         "annotations": df_annotations.to_dict(orient="records"),
     }
 
-    with open(args.eda_out, "w") as f:
+    with open(args.eda_out, "w", encoding="utf-8") as f:
         json.dump(result_dict, f, indent=4)
         print("Data is saved to:", args.eda_out)
