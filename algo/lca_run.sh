1b4b33a6505d517f92a8c0dcabb3125417e131a7
Kate Nepovinnykh <kwadraterry@gmail.com>
2025-05-29 19:23:41 -0400

    Fix typo

--- algo/lca_run.sh
+++ algo/lca_run.sh
@@ -4,22 +4,24 @@
 
 set -e
 
-ALG_NAME = "hdbscan"
+ALG_NAME="hdbscan"
 IMAGES_DIR=""
 ANNOTATIONS_FILE="test_dataset/annotations_LCA.json"
 EMBEDDINGS_FILE="test_dataset/embeddings_LCA.pickle"
 VERIFIER_PROBS="test_dataset/verifiers_probs.json"
-DB_DIR="test_dataset/lca/db"
-EXP_NAME="zebra_drone"
+DB_PATH="test_dataset/lca/db"
+OUTPUT_DIR="test_dataset/output"
+EXP_NAME="zebra_drone_db"
 LOG_FILE="test_dataset/lca/drone_log.log"
 
 python3 algo/lca.py \
     "$LCA_DIR" \
     "$IMAGES_DIR" \
+    "$OUTPUT_DIR" \
     "$ANNOTATIONS_FILE" \
     "$EMBEDDINGS_FILE" \
     "$VERIFIER_PROBS" \
-    "$DB_DIR" \
+    "$DB_PATH" \
     "$LOG_FILE" \
     "$EXP_NAME" \
     "$ALG_NAME" \
